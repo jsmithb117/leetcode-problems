e38e22d2c1be69dc8217a0e5f0e56d3fab44e013
jsmithb117 <jsmithb117@gmail.com>
2020-07-08 16:02:23 -0700

    Completed repeatedNTimes

--- judgeCircle.js
+++ judgeCircle.js
@@ -3,33 +3,23 @@ var judgeCircle = function(moves) {
   var downs = 0;
   var lefts = 0;
   var rights = 0;
-  //convert 'moves' into separate letters
   var movesArray = moves.split('');
-  //iterate those letters
   for (var i = 0; i < movesArray.length; i++) {
-    //count ups
     if (movesArray[i] === 'U') {
       ups++;
     } else if (movesArray[i] === 'D') {
-      //count downs
       downs++;
     } else if (movesArray[i] === 'L') {
-      //count lefts
       lefts++;
     } else if (movesArray[i] === 'R') {
-      //count rights
       rights++;
     } else {
       console.log ('Error!!!')
     }
   }
-  //if ups == downs and lefts == rights
   if ((ups === downs) && (rights === lefts)) {
-    //return true
     return true;
-    //else
   } else {
-    //return false
     return false
   }
 };
